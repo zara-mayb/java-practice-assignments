da7c4c4a7e30d0091cdae8f62be79dc3981ee3b7
Zara <zaramaybernardo@gmail.com>
2023-06-05 16:44:03 -0400

    first commit

---
+++ first_java_program/Intro.java
@@ -0,0 +1,7 @@
+public class Intro{
+    public static void main(String[] args){
+        System.out.println("My name is Zara");
+        System.out.println("I am 23 years old");
+        System.out.println("I am from the Philippines");
+    }
+}
