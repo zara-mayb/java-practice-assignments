e7f250005e27c137d6c8e09f76867145233b994a
Zara May Bernardo <119703721+zara-mayb@users.noreply.github.com>
2023-06-13 23:14:55 -0400

    first commit

---
+++ counter/src/main/java/com/zmb/counter/controllers/HomeController.java
@@ -0,0 +1,27 @@
+package com.zmb.counter.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import jakarta.servlet.http.HttpSession;
+
+@Controller
+public class HomeController {
+	@RequestMapping("/")
+	public String index(HttpSession session) {
+		Integer count = 0;
+		if(session.getAttribute("count")==null) {
+			session.setAttribute("count", 0);
+		}else {
+			count = (Integer) session.getAttribute("count");
+			count++;
+			session.setAttribute("count", count);
+		}
+		
+		return "index.jsp";
+	}
+	@RequestMapping("/counter")
+	public String counter() {
+		return "counter.jsp";
+	}
+}
